c0d3c61172393ae213e672ba7028bf3476ced28c
mahi <maheshvengala4321@gmail.com>
2024-07-05 12:16:05 +0530

    multiple messages

--- pyth.py
+++ pyth.py
@@ -9,14 +9,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
-driver.maximize_window()
+# driver.maximize_window()
 driver.start_session
 
 phone_number = ['9381574024','9866810344','7702420478','6303060078','9182086295','7981663360','8498821093']
-message = 'Hello, this is a test message!'
+messages = 'Hello, this is a test message!'
 
 
-def send_msg_to_phone_number(i):
+def send_msg_to_phone_number(i,message):
     url = f'https://web.whatsapp.com/send?phone=+91{i}&text={message}'
     driver.get(url)
 
@@ -25,10 +25,14 @@ def send_msg_to_phone_number(i):
     send_button = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button')))
     send_button.click()
     
+j=1
 for i in phone_number:
     
     time.sleep(10)
 
-    send_msg_to_phone_number(i)
+    message = messages+str(j)
+    j+=1
+    send_msg_to_phone_number(i, message)
+    print(message,i)
 
 driver.quit
